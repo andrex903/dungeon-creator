9ef3d68406850d135c5da9be9e506cef14264c31
andrex903 <50050676+andrex903@users.noreply.github.com>
2023-05-15 18:22:54 +0200

    Update DungeonCreatorWindow.cs

--- Editor/Windows/DungeonCreatorWindow.cs
+++ Editor/Windows/DungeonCreatorWindow.cs
@@ -206,7 +206,7 @@ namespace RedeevEditor.DungeonCreator
                 if (prefabs.Count == 0) continue;
 
                 Room prefab = prefabs[UnityEngine.Random.Range(0, prefabs.Count)];
-                Vector3 position = room.boundCenter;
+                Vector3 position = GetBoundCenter(room.gameObject);
                 Delete(room);
                 CreateRoom(prefab, block, position);
             }
